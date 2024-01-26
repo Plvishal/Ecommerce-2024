bc75c8123cce9c317aab6f1931d0147cdf5355f8
Vishal <vishalpal1501@gmail.com>
2024-01-26 22:04:18 +0530

    Admin profile done

--- frontend/src/components/adminDashboard/profile/AdminProfile.jsx
+++ frontend/src/components/adminDashboard/profile/AdminProfile.jsx
@@ -1,5 +1,39 @@
+import { useContext } from 'react';
+import UserContext from '../../../context/userContext';
+
 function AdminProfile() {
-  return <div>AdminProfile</div>;
+  const { admin } = useContext(UserContext);
+  console.log(admin);
+  return (
+    <>
+      <div>
+        <div>
+          <div className="w-full h-full">
+            <img
+              src={admin.profileImg.url}
+              alt="admin"
+              className=" w-auto h-44 m-auto rounded-xl border p-1"
+            />
+          </div>
+          <hr className="text-white" />
+          <div className="text-white">
+            <h4>
+              <strong>Name:&nbsp;&nbsp;</strong>
+              {admin.name}
+            </h4>
+            <h4>
+              <strong>Email ID:&nbsp;&nbsp;</strong>
+              {admin.email}
+            </h4>
+            <h4>
+              <strong>Role:&nbsp;&nbsp;</strong>
+              {admin.role}
+            </h4>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AdminProfile;
